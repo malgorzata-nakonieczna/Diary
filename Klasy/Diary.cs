25369d9b511de53ffa74d851ef7ebbf7bc4d1e33
Małgorzata Nakonieczna <m.k.nakonieczna@gmail.com>
2020-12-21 00:04:54 +0100

    added class Diary

---
+++ Klasy/Diary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Klasy
+{
+    class Diary
+    {
+        //dane
+        List<float> ratings;  //zadeklarowanie listy ocen
+
+        //zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating); //metoda dodaje oceny do listy ocen
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+
+            }
+            avg = sum / ratings.Count;
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ Klasy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Klasy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
